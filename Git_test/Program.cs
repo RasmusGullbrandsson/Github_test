fba4e88f31d4e72a6c03ff7041250a3455c1d9b2
unknown <rasmuscg@hotmail.com>
2022-09-16 16:58:48 +0200

    La till en metod

--- Git_test/Program.cs
+++ Git_test/Program.cs
@@ -1,1 +1,12 @@
 ﻿Console.WriteLine("Labb 1 - Versionhantering");
+
+void Veckodagar()
+{
+    Console.WriteLine("Måndag");
+    Console.WriteLine("Tisdag");
+    Console.WriteLine("Onsdag");
+    Console.WriteLine("Torsdag");
+    Console.WriteLine("Fredag");
+    Console.WriteLine("Lördag");
+    Console.WriteLine("Söndag");
+}
